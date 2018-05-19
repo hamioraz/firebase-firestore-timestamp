06aeb1700b3d16ab9d0a04e9b22167c357d59661
gareth <gareth@islandboy.com.au>
2018-05-19 16:31:03 +1000

    feat: export Timestamp is now a class

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-exports.Timestamp = function() {
-    function e(e, t) {
+class Timestamp {
+    constructor(e, t) {
         if (e && e.e) e = e.e;
         if (e && e.nanoseconds) t = e.nanoseconds;
         if (e && e.seconds) e = e.seconds;
@@ -7,24 +7,42 @@ exports.Timestamp = function() {
         if (this.seconds = e, this.nanoseconds = t, t < 0) throw new FirestoreError(Code.INVALID_ARGUMENT, "Timestamp nanoseconds out of range: " + t);
         if (t >= 1e9) throw new FirestoreError(Code.INVALID_ARGUMENT, "Timestamp nanoseconds out of range: " + t);
         if (e < -62135596800) throw new FirestoreError(Code.INVALID_ARGUMENT, "Timestamp seconds out of range: " + e);
-        if (e >= 253402300800) throw new FirestoreError(Code.INVALID_ARGUMENT, "Timestamp seconds out of range: " + e)
+        if (e >= 253402300800) throw new FirestoreError(Code.INVALID_ARGUMENT, "Timestamp seconds out of range: " + e);
     }
-    return e.now = function() {
-        return e.fromMillis(Date.now())
-    }, e.fromDate = function(t) {
-        return e.fromMillis(t.getTime())
-    }, e.fromMillis = function(t) {
+
+    static now() {
+        return this.fromMillis(Date.now())
+    };
+
+    static fromDate(t) {
+        return this.fromMillis(t.getTime())
+    };
+
+    static fromMillis(t) {
         var n = Math.floor(t / 1e3);
-        return new e(n, 1e6 * (t - 1e3 * n))
-    }, e.prototype.toDate = function() {
-        return new Date(this.toMillis())
-    }, e.prototype.toMillis = function() {
-        return 1e3 * this.seconds + this.nanoseconds / 1e6
-    }, e.prototype._compareTo = function(e) {
-        return this.seconds === e.seconds ? primitiveComparator(this.nanoseconds, e.nanoseconds) : primitiveComparator(this.seconds, e.seconds)
-    }, e.prototype.isEqual = function(e) {
-        return e.seconds === this.seconds && e.nanoseconds === this.nanoseconds
-    }, e.prototype.toString = function() {
-        return "Timestamp(seconds=" + this.seconds + ", nanoseconds=" + this.nanoseconds + ")"
-    }, e
-}();
+        return new this(n, 1e6 * (t - 1e3 * n))
+    };
+}
+
+Timestamp.prototype.toDate = function() {
+    return new Date(this.toMillis())
+};
+
+Timestamp.prototype.toMillis = function() {
+    return 1e3 * this.seconds + this.nanoseconds / 1e6
+};
+
+Timestamp.prototype._compareTo = function(e) {
+    return this.seconds === e.seconds ? primitiveComparator(this.nanoseconds, e.nanoseconds) : primitiveComparator(this.seconds, e.seconds)
+};
+
+Timestamp.prototype.isEqual = function(e) {
+    return e.seconds === this.seconds && e.nanoseconds === this.nanoseconds
+};
+
+Timestamp.prototype.toString = function() {
+    return "Timestamp(seconds=" + this.seconds + ", nanoseconds=" + this.nanoseconds + ")"
+};
+
+module.exports = Timestamp;
+
